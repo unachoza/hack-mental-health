0551e8729079ff8d8b80d7a90dfe966c0ba4f6d7
Arianna Choza <unachoza@gmail.com>
2019-03-23 16:57:35 -0700

    passion journal frame

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react'
+import '../App.css'
 
 class App extends Component {
     render() {
-        return <div>Welcome to Hack Mental Health</div>
+        return (
+            <div>
+                <h1>Passion Journal</h1>
+                <div className="journal-list">
+                    <div className="day">
+                        <h3></h3>
+                        <div className="title">
+                            <div className="body">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
     }
 }
 

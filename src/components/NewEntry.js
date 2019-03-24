f0915e33da0f237091e7b2dd156e9c219a7e3be3
Arianna Choza <unachoza@gmail.com>
2019-03-24 06:01:18 -0700

    added to entry object. added entries array

--- src/components/NewEntry.js
+++ src/components/NewEntry.js
@@ -2,14 +2,50 @@ import React, { Component } from 'react'
 import './NewEntry.css'
 
 class NewEntry extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            entry: 'Today I feel',
+            post: "",
+            entries: []
+                
+        }
+    }
+
+    handleJournalInput = (e) => {
+        this.setState({ entry: e.target.value })
+    }
+
+    handleJournalSubmit = (e) => {
+        console.log('this is being submited')
+        this.setState({ post: e.target.value })
+          
+        e.preventDefault()
+        console.log(this)
+        const entry = {
+            title: this.entryTitle.value,
+            text: this.entryText.value
+        }
+        const newEntry = Array.from(this.state.entries)
+        newEntry.push(entry)
+        this.setState({
+            entries: newEntry
+        })
+        this.entryTitle.value = "",
+            this.entryText.value = "",
+    }
+  
     render() {
         return (
-            <form method="post" action="POST">
-                
-                <textarea name="text" placeholder="Today I feel ... " id="text" cols="47" rows="10"></textarea>
-                 <br />
-                <input type="text"></input>
-            </form>
+            <div>
+                <h1>New Entry</h1>
+                <form onSubmit={this.handleJournalSubmit}>
+                    
+                    <textarea onChange={this.handleJournalInput} name="text"  id="text" cols="47" rows="10">{this.state.entry}</textarea>
+                    <br />
+                    <input className="submit-button" type="submit" value="Submit"></input>
+                </form>
+            </div>
         )
     }
 }
